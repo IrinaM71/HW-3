d94f0dde93530d6b665da9dd1e7a75bb5fa19fa2
IrinaM71 <git@github.com>
2025-07-02 17:53:08 +0200

    Выполнила первое задание. Не работают две картинки

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,38 +1,37 @@
-import React, { useState } from "react";
+import { useState } from "react";
+
+const ratingList = [
+  "https://www.clipartmax.com/middle/m2i8H7A0A0Z5A0K9_5-star-rating-png",
+
+  "https://www.clipartmax.com/png/small/67-676956_out-of-3-out-of-5-star-rating.png",
+
+  "https://www.clipartmax.com/png/small/73-731815_stars-clipart-2-5-star-rating.png",
+
+  "https://www.clipartmax.com/png/small/297-2970338_rating-2-out-of-5-gold-stars.png",
+];
+
+const ratingLabels = ["Отлично", "Хорошо", "Приемлимо", "Плохо"];
 const Rating = () => {
   const [ratingValue, setRatingValue] = useState(0);
 
-  const ratingList = [
-    "https://www.clipartmax.com/middle/m2i8H7A0A0Z5A0K9_5-star-rating-png",
-
-    "https://www.clipartmax.com/png/small/67-676956_out-of-3-out-of-5-star-rating.png",
-
-    "https://www.clipartmax.com/png/small/73-731815_stars-clipart-2-5-star-rating.png",
-
-    "https://www.clipartmax.com/png/small/297-2970338_rating-2-out-of-5-gold-stars.png",
-  ];
-
-  const handleRatingValue = (value) => {
-    setRatingValue(value);
-  };
-
   return (
-    <div>
+    <div className="rating-container">
       <h2>Оценка</h2>
       <img
         src={ratingList[ratingValue]}
-        alt={"Рейтинг: ${ratingValue}"}
-        style={{ width: "150px", height: "150px" }}
+        alt={"Рейтинг: ${ratingLabels[ratingValue]}"}
+        className="rating-image"
       />
-      <div style={{ marginTop: "10px" }}>
-        <button onClick={() => handleRatingValueratingChange(1)}>Плохо</button>
-        <button onClick={() => handleRatingValueratingChange(2)}>
-          Приемлимо
-        </button>
-        <button onClick={() => handleRatingValueratingChange(3)}>Хорошо</button>
-        <button onClick={() => handleRatingValueratingChange(4)}>
-          Отлично
-        </button>
+      <div className="button-group">
+        {ratingLabels.map((label, index) => (
+          <button
+            key={index}
+            onClick={() => setRatingValue(index)}
+            className="rating-button"
+          >
+            {label}
+          </button>
+        ))}
       </div>
     </div>
   );
